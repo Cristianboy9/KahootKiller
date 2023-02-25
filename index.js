1063155bf1091804950b3c9d6b069d050eb9f611
Cristianboy9 <97681113+Cristianboy9@users.noreply.github.com>
2023-02-25 14:25:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ console.log(' ')
 var Kahoot = require("kahoot.js-latest");
 var colors = require('colors');    
 const prompt = require('prompt-sync')();
-const name = prompt('Nombre de los Bots: ')
-const pin = prompt('Kahoot PIN: ');
+const name = prompt('Nome of Bots: ')
+const pin = prompt('Game PIN: ');
 number = 1
 number2 = 1
 setInterval(kahoot, 820)
 function kahoot() {
 const client = new Kahoot;
 client.join(pin, name + (number++))
-client.on("Unido", () => {
- console.log("Bot enviado: " + number2++);
+client.on("Joined", () => {
+ console.log("Sended Bot: " + number2++);
 }); 
 
 client.on("QuizEnd", () => {
-  console.log("Juego terminado.");
+  console.log("Game Ended.");
 });
 }
